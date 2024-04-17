b65f34df78a81ad787bb54e7daee0aab857f4da6
Aborot11 <astralshiro26@gmail.com>
2024-04-17 21:43:47 +0800

    update

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from studentorg.models import Organization
 
+class HomepageView(ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = "home.html"
+
+class OrganizationnList(ListView) : 
+    model = Organization
+    context_object_name = 'organzation'
+    template_name = 'organization'
+    paginate_by = 5
 # Create your views here.
