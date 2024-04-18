b627e41fefa703770a842a0fbacaf59c15c50eac
Mendoza1411 <generalmendoza141@gmail.com>
2024-04-19 00:46:52 +0800

    Modified

--- projectsite/studentorg/management/commands/create_initial_data.py
+++ projectsite/studentorg/management/commands/create_initial_data.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from faker import Faker
 from studentorg.models import College, Program, Organization, Student, OrgMember
 
-class Command(BaseCommand):
+class Command (BaseCommand):
     help = 'Create initial data for the application'
 
     def handle(self, *args, **kwargs):
@@ -14,38 +14,34 @@ class Command(BaseCommand):
         fake = Faker()
 
         for _ in range(count):
-            words = [fake.word() for _ in range (2) ] #two words
-            organization_name = ''.join(words)
+            words = [fake.word() for _ in range(2)]
+            organization_name = ' ' .join(words)
             Organization.objects.create(
-            name=organization_name.title(),
-            college=College.objects.order_by ('?').first(),
-            description=fake.sentence()
-        )
+                name=organization_name.title(),
+                college=College.objects.order_by('?').first(),
+                description=fake.sentence()
+            )
+        
+        self.stdout.write(self.style.SUCCESS('Initial data for organization created successfully.'))
 
-        self.stdout.write(self.style.SUCCESS(
-        'Initial data for organization created sucessfully.'))
-    
     def create_students(self, count):
         fake = Faker('en_PH')
         for _ in range(count):
-            Student.objects.create(
-                student_id=f"{fake.random_int(2020,2024)}-{fake.random_int(1,8)}-{fake.random_number(digits=4)}",
-                lastname=fake.last_name(),
-                firstname=fake.first_name(),
-                middlename=fake.last_name(),
-                program=Program.objects.order_by('?').first()
+            Student.objects.create(student_id=f"{fake.random_int(2020, 2024)}-{fake.random_int(1,8)}-{fake.random_number(digits=4)}",
+                                   lastname=fake.last_name(),
+                                   firstname=fake.first_name(),
+                                   middlename=fake.last_name(),
+                                   program=Program.objects.order_by('?').first()
             )
-        self.stdout.write(self.style.SUCCESS(
-            'Initial data for students created successfully.'))
-        
+        self.stdout.write(self.style.SUCCESS('Initial data for students created successfully'))
+
     def create_membership(self, count):
         fake = Faker()
         for _ in range(count):
             OrgMember.objects.create(
-                students=Student.objects.order_by('?').first(),
-                organization=Organization.Objects.order_by('?').first(),
+                student=Student.objects.order_by('?').first(),
+                organization=Organization.objects.order_by('?').first(),
                 date_joined=fake.date_between(
                     start_date="-2y", end_date="today")
-                )
-        self.stdout.write(self.style.SUCCESS(
-            'Initial data for student organization create successfully.'))
+            )
+        self.stdout.write(self.style.SUCCESS('Initial data for student organization created successfully.'))
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,62 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-<<<<<<< HEAD
 from studentorg.models import Organization
 
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
-=======
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from studentorg.models import Organization
-from studentorg.forms import OrganizationForm
-from django.urls import reverse_lazy
-from typing import Any
-from django.db.models.query import QuerySet
-from django.db.models import Q
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
-
-
-
-@method_decorator(login_required, name='dispatch')
-class HomePageView(ListView):
-    model = Organization
-    context_object_name = 'home'
-    template_name = "home.html"
-
-# Create your views here.
-class OrganizationList(ListView):
-    model = Organization
-    context_object_name = 'organization'
-    template_name  = 'org_list.html'
-    paginate_by = 5
-
-    def get_queryset(self, *args, **kwargs):
-         qs = super(OrganizationList, self).get_queryset(*args, **kwargs)
-         if self.request.GET.get("q") != None:
-             query = self.request.GET.get('q')
-             qs = qs = qs.filter(Q(name__icontains=query) |
-                           Q(description__icontains=query) | Q(college__college_name__icontains=query))
-         return qs
-
-
-class OrganizationCreateView(CreateView):
-    model = Organization
-    form_class = OrganizationForm
-    template_name = 'org_add.html'
-    success_url = reverse_lazy('organization-list')
-
-class OrganizationUpdateView(UpdateView):
-    model = Organization
-    form_class = OrganizationForm
-    template_name = 'org_edit.html'
-    success_url = reverse_lazy('organization-list')
-
-class OrganizationDeleteView(DeleteView):
-    model = Organization
-    template_name = 'org_del.html'
-    success_url = reverse_lazy('organization-list')
-    
->>>>>>> 7710e4cb76d4c360b55336abced7aa947a6b7b0d
